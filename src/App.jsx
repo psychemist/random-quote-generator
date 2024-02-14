81a5ff3a88f1421d6ffc54a9ae6ac5609827adc8
psych0wh1z <ikeagudike@gmail.com>
2024-02-14 03:15:46 +0100

    Add element skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 function App() {
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <wrapper id='quote-box'>
+      <div id='text'></div>
+      <p id="author"></p>
+      <button id="new-quote"></button>
+    </wrapper>
+  )
 }
 
 export default App
